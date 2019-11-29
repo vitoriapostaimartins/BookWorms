7a8e0f3b749697692b5574a630fe915abacb6335
Vitória Martins <vitoriamartins@vitorias-air.wifi.bcit.ca>
2019-11-29 11:50:19 -0800

    cleaned up version

--- files/scripts/app.js
+++ files/scripts/app.js
@@ -2,47 +2,11 @@
 // This files contains all the javascript functions
 // used in the project
 //
-// @author Carly Orr
+// @author Christina Raganit
+// @author Vitoria Postai Martins
 // @version 1.0
 //=================================================
 
-// ---------------------------------------------
-// Display the current Caffeine Count (id="caffeinecount")
-// ---------------------------------------------
-//function showCaffeineCount(){
-//  //var x=25;  // get this number from database eventually
-//  // for now we assume this value is in localStorage
-//  var x = window.localStorage.getItem('num_cups');
-//  document.getElementById("caffeinecount").innerHTML = x;
-//}
-
-// ---------------------------------------------
-// Display the current Date (id="today")
-// ---------------------------------------------
-//function showDate(){
-//  var d = new Date();
-//  var line = d.toDateString().slice(0,15);
-//  document.getElementById("today").innerHTML = line;
-//}
-
-// ---------------------------------------------
-// Setup a Listener for the Logout button called "btnLogout"
-// When button is clicked, increment the "num_cups" variable.
-// Eventually this is saved in the database.
-// For now, we save it into LocalStorage.
-// ---------------------------------------------
-//function setAddListener(){
-//  document.getElementById("addBtn").addEventListener("click", function(e) {
-//    var x= parseInt(window.localStorage.getItem("num_cups"));
-//    window.localStorage.setItem('num_cups', x+1);
-//    showCaffeineCount();
-//  })
-//}   
-
-// ---------------------------------------------
-// If the currently logged in user is authenticated,
-// then signout this person "user".
-// ---------------------------------------------
 function logoutUser() {
   firebase.auth().onAuthStateChanged(function(user){
       var promise = firebase.auth().signOut();
@@ -78,6 +42,7 @@ function showBooks(){
                     console.log (doc.id); // 
                     var bookid = doc.id;
                     console.log(book);
+                    
                     //creates book div
                     var bookdiv = document.createElement('tr');
                     bookdiv.setAttribute('class', 'book');
@@ -101,20 +66,19 @@ function showBooks(){
                     var title = document.createElement('p');
                     title.innerHTML = book.title;
                     title.setAttribute('class', 'bookTitle');
+                    
                     //adds title to book cover
                     bookcover.appendChild(title);
                     var favs = document.createElement('button');
-//                    favs.setAttribute('id', 'button' + i);
                     favs.setAttribute('class', 'favs');
                     favs.setAttribute('id', 'button' + doc.id.toString());
                     favs.innerHTML = 'Add to Favorites';
-                    //favs.setAttribute('onclick', "this.addEventListener('click', '');");
-//                    favs.addEventListener('click', "console.log('hi', this);");
                     bookcover.appendChild(favs);
                     
                     //creates book details div
                     var bookdetails = document.createElement('td');
                     bookdetails.setAttribute('class', 'details');
+                    
                     //adds book details to book div
                     bookdiv.appendChild(bookdetails);
                     
@@ -202,15 +166,15 @@ function showFavs(){
                     var title = document.createElement('p');
                     title.innerHTML = book.title;
                     title.setAttribute('class', 'bookTitle');
+                
                     //adds title to book cover
                     bookcover.appendChild(title);
-                    //favs.setAttribute('onclick', "this.addEventListener('click', '');");
-//                  //favs.addEventListener('click', "console.log('hi', this);");
                     
                     
                     //creates book details div
                     var bookdetails = document.createElement('td');
                     bookdetails.setAttribute('class', 'details');
+                
                     //adds book details to book div
                     bookdiv.appendChild(bookdetails);
                     
--- files/scripts/create_user.js
+++ files/scripts/create_user.js
@@ -1,8 +1,4 @@
 function createUser(){
-
-    // if the user is authenticated, get this "user" object
-    // create this user node(doc) in the datebase users collection
-
     firebase.auth().onAuthStateChanged(function(user){
         db.collection("users").doc(user.uid).set(
 		{
@@ -10,7 +6,6 @@ function createUser(){
          "email":user.email,
         });
     });
-    //window.localStorage.setItem('num_cups', 0);
 }
 
 
